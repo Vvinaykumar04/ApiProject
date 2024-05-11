67b2c2e3dcdf672757dd98ab22d30e4883ef1bd3
vinay <vinaykgupta04@gmail.com>
2024-05-11 11:43:22 -0700

    Changes in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href="register.html">Register!</a>
                                     </div>
                                 </div>
                             </div>
